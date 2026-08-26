da0ecd954695c566b3b594a30a2e7b24819ab2f4
Tommy Ju <tommyju25@gmail.com>
2026-08-25 19:25:08 -0700

    Setup views and url paths

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Django templates are implicitly loaded from templates/
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
