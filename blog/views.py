e6d931a81b19aeb79594e251b2ee4be5244811ba
Tommy Ju <tommyju25@gmail.com>
2026-08-27 18:49:10 -0700

    Implement feature to save posts as a draft and publish

--- blog/views.py
+++ blog/views.py
@@ -19,7 +19,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else :
@@ -36,7 +35,6 @@ def post_edit(request, pk):
             # Saving the form with commit False retrieves the 'post' model object
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             # Persists the post object in the DB
             post.save()
             return redirect('post_detail', pk=post.pk)            
@@ -44,3 +42,13 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
     
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        post.publish()
+    return redirect('post_detail', pk=pk)
